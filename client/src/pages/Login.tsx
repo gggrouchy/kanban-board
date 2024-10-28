8b6e13e4a9ef3057fb38d6de95a3737394741916
Oscar Ayala <oayala544@gmail.com>
2024-10-27 20:22:30 -0400

    upadated files

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,4 @@
 import { useState, FormEvent, ChangeEvent } from "react";
-
 import Auth from '../utils/auth';
 import { login } from "../api/authAPI";
 
@@ -20,8 +19,12 @@ const Login = () => {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      const data = await login(loginData);
-      Auth.login(data.token);
+      const data: { success: boolean; message?: string; token?: string } = await login(loginData);
+      if (data.success && data.token) {
+        Auth.login(data.token); // Save the JWT and redirect using AuthService
+      } else {
+        console.error('Login failed', data.message);
+      }
     } catch (err) {
       console.error('Failed to login', err);
     }
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,28 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  // Retrieve the token from the "Authorization" header
+  const authHeader = req.headers.authorization;
+
+  // Check if the token is missing
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    
+    const secretKey = process.env.JWT_SECRET || '';
+
+  // Verify the token
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid or expired token' });
+    }
+
+    // Attach the decoded payload to the request object
+    req.user = user as JwtPayload;
+    return next();
+  });
+} else {
+  res.sendStatus(401); // Unauthorized
+}
 };
+
+
